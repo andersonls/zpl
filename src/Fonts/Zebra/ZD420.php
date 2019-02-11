3772dd1048894c6aea0a1cd7307ce6328410230c
Andrew Koebbe <abk@stowers.org>
2019-02-11 16:24:34 -0600

    Add Zebra font mapper

---
+++ src/Fonts/Zebra/ZD420.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zpl\Fonts\Zebra;
+
+class ZD420 extends \Zpl\Fonts\AbstractMapper
+{
+    public static $mapper = array(
+        'Font A' => 'A',
+        'Font B' => 'B',
+        'Font C' => 'C',
+        'Font D' => 'D',
+        'Font E' => 'E',
+        'Font F' => 'F',
+        'Font G' => 'G',
+        'Font H' => 'H',
+        'Font 0' => 0,
+        'Font GS' => 'GS',
+        'Font P' => 'P',
+        'Font Q' => 'Q',
+        'Font R' => 'R',
+        'Font S' => 'S',
+        'Font T' => 'T',
+        'Font U' => 'U',
+        'Font V' => 'V',
+    );
+}
