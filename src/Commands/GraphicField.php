9e0445124f1a3255178a71f31bb08b8fe50e37f6
Anderson <andersonlsilverio@gmail.com>
2022-01-06 14:29:12 -0300

    Removed obsolet copyright block.

--- src/Commands/GraphicField.php
+++ src/Commands/GraphicField.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @author    Anderson Luiz Silvério <andersonlsilverio@gmail.com>
- * @author    Emerson Casas Salvador <salvaemerson@gmail.com>
- *
- * @license   http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
- * @copyright 2019 Digicart Indústria e Com de Circuitos Impressos LTDA
- */
 
 namespace Zpl\Commands;
 
