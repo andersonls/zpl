5dc997e9df28a858fa5134d97772e1598f60f5b6
erikn69 <erikn_69@hotmail.com>
2025-11-04 15:14:10 -0500

    `utf8_decode` deprecated

--- src/PdfBuilder.php
+++ src/PdfBuilder.php
@@ -206,9 +206,34 @@ class PdfBuilder extends AbstractBuilder
     {
         if (extension_loaded('iconv')) {
             return iconv('UTF-8', 'ISO-8859-1', $str);
-        } else {
-            return utf8_decode($str);
         }
+        if (extension_loaded('mbstring')) {
+            return mb_convert_encoding($string, 'ISO-8859-1', 'UTF-8');
+        }
+
+        $s = (string) $str;
+        $len = \strlen($s);
+
+        for ($i = 0, $j = 0; $i < $len; ++$i, ++$j) {
+            switch ($s[$i] & "\xF0") {
+                case "\xC0":
+                case "\xD0":
+                    $c = (\ord($s[$i] & "\x1F") << 6) | \ord($s[++$i] & "\x3F");
+                    $s[$j] = $c < 256 ? \chr($c) : '?';
+                    break;
+                case "\xF0":
+                    ++$i;
+                    // no break
+                case "\xE0":
+                    $s[$j] = '?';
+                    $i += 2;
+                    break;
+                default:
+                    $s[$j] = $s[$i];
+            }
+        }
+    
+        return substr($s, 0, $j);
     }
     
     /**
